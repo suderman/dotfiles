857e644fca6e781e32bcf2a50de3c86e486a256b
Jon Suderman <suderman@gmail.com>
2014-06-18 01:27:34 -0600

    plex dockerized

--- ubuntu/docker/plex/start.sh
+++ ubuntu/docker/plex/start.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 
+# -------------------------------------------
+# Start common services
+# -------------------------------------------
+
+# Start incrond to watch /config/restart.txt
+/usr/sbin/incrond
+
+# Start sshd
+/usr/sbin/sshd
+
+
+# -------------------------------------------
+# Copy config files to where they're expected
+# -------------------------------------------
+
+# Copy config files to where they're expected
+# (none)
+
+
+# -------------------------------------------
+# Start this container's services
+# -------------------------------------------
+
+# Clean-up
 rm -f "/config/Library/Application Support/Plex Media Server/plexmediaserver.pid"
 
+# Start the service
 HOME=/config start_pms &
 sleep 5
 
-tail -f /config/Library/Application\ Support/Plex\ Media\ Server/Logs/**/*.log
+# Tail the logs and keep the container alive
+tail -F /config/Library/Application\ Support/Plex\ Media\ Server/Logs/**/*.log
