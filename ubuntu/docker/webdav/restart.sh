5e41f64c34e6abc4bd5de27e5cde36e8273360f9
Jon Suderman <suderman@gmail.com>
2014-06-26 16:12:25 -0600

    Better WebDav data web browsing

--- ubuntu/docker/webdav/restart.sh
+++ ubuntu/docker/webdav/restart.sh
@@ -4,6 +4,12 @@
 # Copy config files to where they're expected
 # -------------------------------------------
 
+# Write the env variables if set
+[ -z "$WEBDAV_USER" ] || echo $WEBDAV_USER > /config/webdav_user.txt
+[ -z "$WEBDAV_PASS" ] || echo $WEBDAV_PASS > /config/webdav_pass.txt
+$WEBDAV_USER=$(cat /config/webdav_user.txt)
+$WEBDAV_PASS=$(cat /config/webdav_pass.txt)
+
 # Copy config files to where they're expected
 cp -f /config/webdav.conf /etc/apache2/sites-available/000-default.conf
 sed -i.bak "s/\$WEBDAV_USER/$WEBDAV_USER/g" /etc/apache2/sites-available/000-default.conf
