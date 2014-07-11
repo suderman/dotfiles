058fb5df12632dfba7916c96c801a9d0cb6e51aa
Jon Suderman <suderman@gmail.com>
2014-07-10 22:32:42 -0600

    Certificate Authority (WIP)

---
+++ ubuntu/docker/ca/revoke.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+source /helper.sh
+
+# -------------------------------------------
+# Set environment variables
+# -------------------------------------------
+
+CA_DOMAIN=`getenv CA_DOMAIN localhost`
+CA_NAME=`getenv CA_NAME 'Certificate Authority'`
+COUNTRY=`getenv COUNTRY $(curl -s ipinfo.io/country)`
+REGION=`getenv REGION $(curl -s ipinfo.io/region)`
+CITY=`getenv CITY $(curl -s ipinfo.io/city)`
+
+
+# -------------------------------------------
+# Copy config files to where they're expected
+# -------------------------------------------
+
+# Copy config files to where they're expected
+cp -f /config/openssl.cnf /usr/lib/ssl/openssl.cnf
+expenv CA_DOMAIN /usr/lib/ssl/openssl.cnf
+expenv CA_NAME /usr/lib/ssl/openssl.cnf
+
+
+# -------------------------------------------
+# New Certificate
+# -------------------------------------------
+
+# Certificate variables
+NAME="$1"
+
+# Stop here if it doesn't exists
+if ! grep '^V.*'`printf '%q' "/CN=$NAME/"` /config/db/index; then
+  echo "Stopped: $NAME doesn't exist."
+  exit; 
+fi
+
+# Revoke the certificate
+openssl ca                                                                              \
+  -revoke /config/certs/$NAME/$NAME.crt                                                 \
+  -keyfile /config/ca/ca.key                                                            \
+  -cert /config/ca/ca.crt                                                               \
+  -config /config/openssl.cnf
+
+# Remove all key/cert files
+rm -rf  /config/certs/$NAME
+
+# Update the certificate revocation list
+openssl ca -gencrl                                                                      \
+  -keyfile /config/ca/ca.key                                                            \
+  -cert /config/ca/ca.crt                                                               \
+  -out /config/crl/ca.crl
---
+++ ubuntu/docker/ca/start.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+source /helper.sh
+
+# -------------------------------------------
+# Start common services
+# -------------------------------------------
+
+# Start incrond to watch /config/restart.txt
+# /usr/sbin/incrond
+
+# Start sshd
+# /usr/sbin/sshd
+
+
+# -------------------------------------------
+# Set environment variables
+# -------------------------------------------
+
+CA_DOMAIN=`getenv CA_DOMAIN localhost`
+CA_NAME=`getenv CA_NAME 'Certificate Authority'`
+COUNTRY=`getenv COUNTRY $(curl -s ipinfo.io/country)`
+REGION=`getenv REGION $(curl -s ipinfo.io/region)`
+CITY=`getenv CITY $(curl -s ipinfo.io/city)`
+
+
+# -------------------------------------------
+# Copy config files to where they're expected
+# -------------------------------------------
+
+# Copy config files to where they're expected
+cp -f /config/openssl.cnf /usr/lib/ssl/openssl.cnf
+expenv CA_DOMAIN /usr/lib/ssl/openssl.cnf
+expenv CA_NAME /usr/lib/ssl/openssl.cnf
+
+
+# -------------------------------------------
+# Start this container's services
+# -------------------------------------------
+
+# Create directory structure
+mkdir -p /config/ca
+mkdir -p /config/db
+mkdir -p /config/crl
+mkdir -p /config/certs
+mkdir -p /config/newcerts
+
+# Flat file database
+[ -f /config/db/index ] || touch /config/db/index
+[ -f /config/db/serial ] || echo 1000 > /config/db/serial
+[ -f /config/crl/crlnumber ] || echo 1000 > /config/crl/crlnumber
+
+# Certificate variables
+ORG="$CA_DOMAIN CA"
+UNIT="Certificate Authority"
+NAME="$CA_NAME"
+EMAIL="ca@$CA_DOMAIN"
+
+# Generate the certificate authority key
+[ -f /config/ca/ca.key ] || 
+openssl genrsa -out /config/ca/ca.key 2048
+chmod 400 /config/ca/ca.key
+
+# Generate the certificate authority certificate
+[ -f /config/ca/ca.crt ] || 
+openssl req -new -x509 -nodes -days 3650 -extensions v3_ca                              \
+  -subj "/C=$COUNTRY/ST=$REGION/L=$CITY/O=$ORG/OU=$UNIT/CN=$NAME/emailAddress=$EMAIL/"  \
+  -key /config/ca/ca.key                                                                \
+  -out /config/ca/ca.crt
+
+# Generate the certificate revocation list
+openssl ca -gencrl                                                                      \
+  -keyfile /config/ca/ca.key                                                            \
+  -cert /config/ca/ca.crt                                                               \
+  -out /config/crl/ca.crl
+
+# ------ END OF START -------- #
+
+
+
+# Start 
+# /usr/sbin/service mysql start
+
+# Tail the logs and keep the container alive
+# tail -F /var/log/mysql.log
