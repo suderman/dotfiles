28e0a2bffae6a63fb76b4f186d2cb109ac83579f
Jon Suderman <suderman@gmail.com>
2015-01-29 20:15:33 -0700

    megachange

---
+++ ubuntu/docker/dnsmasq/restart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source /helper.sh
+
+# -------------------------------------------
+# Set environment variables
+# -------------------------------------------
+
+DOMAIN=`getenv DOMAIN localhost`
+HOST_IP=`getenv HOST_IP localhost`
+GATEWAY_IP=`getenv GATEWAY_IP localhost`
+
+# -------------------------------------------
+# Copy config files to where they're expected
+# -------------------------------------------
+
+# ln -sf /config/dnsmasq.conf /etc/dnsmasq.conf
+ln -sf /config/ethers /etc/ethers
+ln -sf /config/hosts /etc/hosts.local
+ln -sf /config/hosts.dhcp /etc/hosts.dhcp
+
+cp -f /config/dnsmasq.conf  /etc/dnsmasq.conf
+expenv DOMAIN               /etc/dnsmasq.conf
+expenv HOST_IP              /etc/dnsmasq.conf
+expenv GATEWAY_IP           /etc/dnsmasq.conf
+
+# -------------------------------------------
+# Restart this container's services
+# -------------------------------------------
+
+# Restart it
+pkill -HUP dnsmasq
--- ubuntu/docker/plexconnect/start.sh
+++ ubuntu/docker/plexconnect/start.sh
@@ -18,17 +18,23 @@ source /helper.sh
 
 # Copy config files to where they're expected
 touch /config/Settings.cfg && ln -sf /config/Settings.cfg /PlexConnect/Settings.cfg
-rm -rf /PlexConnect/assets/certificates && ln -sf /config/certificates /PlexConnect/assets/
+# rm -rf /PlexConnect/assets/certificates && ln -sf /config/certificates /PlexConnect/assets/
 
 
 # -------------------------------------------
 # Start this container's services                                  :
 # -------------------------------------------
 
-# Generate the certificate needed
-cd /config/certificates
-(has trailers.pem) || openssl req -new -nodes -newkey rsa:2048 -out trailers.pem -keyout trailers.key -x509 -days 7300 -subj "/C=US/CN=trailers.apple.com"
-(has trailers.cer) || openssl x509 -in trailers.pem -outform der -out trailers.cer && cat trailers.key >> trailers.pem
+# Get the server key & pem, and the ca crt & crl
+curl "$CA_SERVER/trailers.apple.com.key" > /nginx/trailers.apple.com.key
+curl "$CA_SERVER/trailers.apple.com.pem" > /nginx/trailers.apple.com.pem
+curl "$CA_SERVER/trailers.apple.com.cer" > /nginx/trailers.apple.com.cer
+chmod 600 /nginx/trailers.apple.com.key /nginx/trailers.apple.com.pem
+
+# # Generate the certificate needed
+# cd /config/certificates
+# (has trailers.pem) || openssl req -new -nodes -newkey rsa:2048 -out trailers.pem -keyout trailers.key -x509 -days 7300 -subj "/C=US/CN=trailers.apple.com"
+# (has trailers.cer) || openssl x509 -in trailers.pem -outform der -out trailers.cer && cat trailers.key >> trailers.pem
 
 # Check for updates
 cd /PlexConnect && /usr/bin/git pull
