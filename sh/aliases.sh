45d891999f4ce819bb3fb7da22bf0407b7a26c4b
Jon Suderman <suderman@gmail.com>
2016-05-09 17:38:52 -0600

    docker aliases

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -45,44 +45,11 @@ if has /usr/local/Cellar/openssl/1.0.1h/bin/openssl; then
   alias ssl="/usr/local/Cellar/openssl/1.0.1h/bin/openssl"
 fi
 
-# docker
-alias dk='docker'
-
-# docker last - get id from last container
-alias dkl='docker ps -l -q'
+# docker compose
+alias dc='docker-compose'
 
 # docker ip - get the ip address from a container
-alias dkip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
-
-# docker remove - remove all stopped containers
-alias dkrm='docker rm $(docker ps --no-trunc -a -q)';
-
-# docker remove images - remove images labeled <none>
-alias dkrmi='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")';
-
-# docker daemon - run an image daemonized
-alias dkd="docker run -d"
-
-# docker terminal - run an image interactively in the terminal
-alias dkt="docker run -i -t"
-
-# docker build - build a tagged image
-dkb() { docker build -t="$1" .; }
-
-# docker stop - stop a specified container, or all of them
-dkstop() { if [[ ! -z ${1} ]]; then docker stop ${1}; else docker stop $(docker ps -q); fi };
-
-# docker clean - stop all containers, remove all containers, remove all images
-alias dkclean='dkstop; dkrm; dkrmi';
-
-# docker images
-alias dki="docker images";
-
-# docker processes
-alias dkps="docker ps -a";
-
-# dockers - all images and processes
-alias dks="docker images; docker ps -a"
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
 
 unalias d 2>/dev/null
 unalias sudo 2>/dev/null
