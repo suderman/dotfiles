3c6f4ec023534f9b7d38389e53cb18a8861ddb3d
Jon Suderman <jon@suderman.net>
2022-01-04 12:50:02 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-# Tools
+# tools
 yay -S fzf tree bat delta
 
-# Dotfiles
-git clone --bare https://github.com/suderman/dotfiles.git $HOME/.dotfiles
-cd $HOME
-alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dot checkout dotfiles
-dot config --local status.showUntrackedFiles no
+# dotfiles
+git clone --bare https://github.com/suderman/dotfiles.git ~/.dotfiles
+git --git-dir=$HOME/.dotfiles config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout dotfiles
 
-# zsh
+# persist git credentials
+git config --global credential.helper store
+
+# zsh plugins
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/kazhala/dotbare.git ~/.oh-my-zsh/custom/plugins/dotbare
 
-# Install plugin manager for nvim and all plugins
+# nvim plugins
 mkdir -p ~/.local/share/nvim/site/autoload
 curl -fL https://github.com/junegunn/vim-plug/raw/master/plug.vim > ~/.local/share/nvim/site/autoload/plug.vim
 nvim -c PlugInstall
 
-# Tmux plugin manager
+# tmux plugins
 mkdir -p ~/.local/share/tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
